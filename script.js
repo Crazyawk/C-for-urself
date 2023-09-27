f5fea06f63a2be46cdfa64ebdab8abb3b10ec064
Crazyawk <143974450+Crazyawk@users.noreply.github.com>
2023-09-27 07:55:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const proxyForm = document.getElementById('proxy-form');
+    const urlInput = document.getElementById('url-input');
+    const resultDiv = document.getElementById('result');
+
+    proxyForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const url = urlInput.value;
+        if (url.trim() === '') {
+            alert('Please enter a valid URL.');
+            return;
+        }
+
+        try {
+            const response = await fetch(`/proxy?url=${encodeURIComponent(url)}`);
+            const data = await response.text();
+            resultDiv.innerHTML = data;
+        } catch (error) {
+            resultDiv.innerHTML = `<p>Error: ${error.message}</p>`;
+        }
+    });
+});
 
